a94ef0b429c86c6e5dbf41729a28fc35bae71c55
Anton Thomas <anton.thomas@student.kdg.be>
2022-12-05 19:28:28 +0100

    :sparkles: Day 6 template

---
+++ day06/index.ts
@@ -0,0 +1,14 @@
+import * as common from '../common/index'
+
+function challenge1() {
+    const lines = common.getFileLines('test.txt');
+    lines.forEach(line => { });
+}
+
+function challenge2() {
+    const lines = common.getFileLines('test.txt');
+    lines.forEach(line => { });
+}
+
+challenge1();
+// challenge2();

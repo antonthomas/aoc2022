b8fd70f106ee38d84d3e4f2aa85e51c997078fc8
antonthomas <antonthomas@users.noreply.github.com>
2022-12-06 08:10:11 +0000

    :poop: stinky code

--- day06/index.ts
+++ day06/index.ts
@@ -1,13 +1,39 @@
 import * as common from '../common/index'
 
+let last4: string[] = [];
+
 function challenge1() {
-    const lines = common.getFileLines('test.txt');
-    lines.forEach(line => { });
+    const line = common.getFileLines('test.txt')[0];
+    let chars = [...line];
+    // last4 = chars.slice(0,2);
+    // chars = chars.slice(2);
+    for (const {c, i} of chars.map((c,i) => ({c,i}))) {
+        if (check(c)) {
+            console.log(i);
+            break;
+        };
+        // console.log(c, last4)
+    }
+}
+
+function check(char: string): boolean {
+    if (last4.length == 4) {
+        let different = true;
+        for (let i = 0; i < 3; i++) {
+            if (last4[i] == last4[3]) return false;
+        }
+        last4 = last4.slice(-1)
+        last4.push(char)
+        console.log(last4)
+        return different;
+    } else {
+        last4.push(char);
+        return false;
+    }
 }
 
 function challenge2() {
-    const lines = common.getFileLines('test.txt');
-    lines.forEach(line => { });
+    const line = common.getFileLines('test.txt')[0];
 }
 
 challenge1();
